f74941dde4bd1a8fbdb12ae3e304838f0f2a68af
dhayaec <dhayaec@gmail.com>
2024-01-10 08:13:06 +0530

    test lint-staged

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,10 +1,10 @@
-import { addAll, reverseStr } from './utils';
+import { addAll, reverseStr } from "./utils";
 
-describe('utils', () => {
-  test('addAll should add all numbers', () => {
+describe("utils", () => {
+  test("addAll should add all numbers", () => {
     expect(addAll([1, 2, 3])).toEqual(6);
   });
-  test('reverseStr', () => {
-    expect(reverseStr('abc')).toEqual(6);
+  test("reverseStr", () => {
+    expect(reverseStr("abc")).toEqual("cba");
   });
 });
